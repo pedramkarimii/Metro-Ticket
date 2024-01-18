e22790b013424e67989c739ca989b689bc3f920c
Pedram Karimi <147748351+pedramkarimii@users.noreply.github.com>
2024-01-18 22:11:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from admin import Admin
-from User import User
-from Bank_Account import BankCreatAccount
+from admin_bank import AdminBank
+from admin_metro import AdminMetro
+from user_bank import UserBank
+from User_metro import UserMetro
+from Pickle import PickleData
+from Show_money_bank import ShowMoneyInfo
 
 
 def main():
     """Main function to run the admin and user interaction loop."""
-    admin_instance = Admin()
-    user_instance = User()
-    account_bank = BankCreatAccount()
+    bank_admin_instance = AdminBank()
+    bank_user_instance = UserBank()
+    metro_admin_instance = AdminMetro()
+    metro_user_instance = UserMetro()
+    pickle_data = PickleData()
+    show_money_info = ShowMoneyInfo()
     while True:
         print("\nEnter a number Options")
         print("Metro or Bank?\n1.Metro\n2.Bank Account")
@@ -27,12 +33,12 @@ def main():
                     national_code = input("Enter your national code: ")
                     phone = input("Enter your phone: ")
                     address = input("Enter your address: ")
-                    user_instance.Add_Information(name, lastname, age, phone, address, national_code)
+                    metro_user_instance.Add_Information(name, lastname, age, phone, address, national_code)
                 elif choice == "2":
                     print("**\nPlease Enter username and password**\n")
-                    username = input("Enter username: ")
-                    password = input("Enter password: ")
-                    if user_instance.check_credentials_User(username, password) == True:
+                    username = "Pedram.9060@gmail.com"  # input("Enter username: ")
+                    password = "Pedram$9060"  # input("Enter password: ")
+                    if metro_user_instance.check_credentials_User(username, password) == True:
                         pass
                 elif choice == "3":
                     print("exit in panel User\n")
@@ -47,8 +53,8 @@ def main():
                     print("**\nPlease Enter username and password**\n")
                     username = input("Enter username: ")
                     password = input("Enter password: ")
-                    if admin_instance.check_credentials(username, password) == True:
-                        pass
+                    if metro_admin_instance.check_credentials(username, password) == True:
+                        pickle_data.pickle_metro_admin_username_password()
                 elif choice == "2":
                     print("exit in panel Admin\n")
                     break
@@ -69,37 +75,37 @@ def main():
                     national_code = input("Enter your national code: ")
                     phone = input("Enter your phone: ")
                     address = input("Enter your address: ")
-                    account_bank.Add_Information_User_Bank(name, lastname, age, phone, address, national_code)
+                    bank_user_instance.Add_Information_User_Bank(name, lastname, age, phone, address, national_code)
 
                 elif choice == "2":
                     print("**\nPlease Enter username and password**\n")
                     username = input("Enter username: ")
                     password = input("Enter password: ")
-                    if account_bank.check_credentials_User_Bank(username, password) == True:
+                    if bank_user_instance.check_credentials_User_Bank(username, password) == True:
                         print("\n1.deposit\n2.withdraw")
                         choice = input("Enter your choice: \n")
                         if choice == "1":
                             amount_deposit = input("Enter your amount for deposit: \n")
-                            account_bank.deposit(amount_deposit)
-                            print("Your balance:", account_bank.get_balance())
+                            bank_user_instance.deposit(amount_deposit)
+                            print("Your balance:", bank_user_instance.get_balance())
                         elif choice == "2":
                             amount_withdraw = input("\nEnter your amount for withdraw: ")
-                            account_bank.withdraw(amount_withdraw)
-                            account_bank.show_money(username)
+                            bank_user_instance.withdraw(amount_withdraw)
+                            show_money_info.show_money(username)
             elif choice == "2":
                 print("**\nPlease Enter username and password**\n")
                 username = input("Enter username: ")
                 password = input("Enter password: ")
-                if account_bank.check_credentials_Admin_Bank(username, password) == True:
+                if bank_admin_instance.check_credentials_Admin_Bank(username, password) == True:
 
                     print("Do you want to Show all account or Search?\n1.Show all account\n2.Search")
                     choice = input("Enter your choice: \n")
                     if choice == "1":
-                        show_all = account_bank.AllMoney
+                        show_all = show_money_info.AllMoney
                         print(f"All account Info: {show_all}")
                     elif choice == "2":
                         name_account = input("Enter name account: ")
-                        all_account = account_bank.AllMoney
+                        all_account = show_money_info.AllMoney
                         search = all_account.keys(name_account)
                         print(search)
                 else:
